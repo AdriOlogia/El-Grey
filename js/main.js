30232be27ab438c8ce13067cb96f8d74c1515dca
adrian747 <eidrian747@gmail.com>
2020-08-31 20:44:04 +0000

    El Grey final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-    $("#btnlogin").click(function(){
-        $('#modal1').modal('show');
+$(document).ready(function(){       
+    var scroll_pos = 0;
+    $(document).scroll(function() { 
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 40) {
+            $("header").css('background-color', 'white');
+        } else {
+            $("header").css('background-color', 'transparent');
+        }
     });
 });
