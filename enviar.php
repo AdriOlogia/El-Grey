b0d781be1aad6e846ed304f3ea34fbb12825c3ae
adrian747 <eidrian747@gmail.com>
2020-09-21 20:58:53 +0000

    ajustes mailerphp

--- enviar.php
+++ enviar.php
@@ -17,7 +17,9 @@ $usuarioname = $_POST['nombre'];
 $usuariosurname = $_POST['apellido'];
 $useremail = $_POST['email'];
 
-
+$body = "El Nombre del Usuario es:   " . $usuarioname . "     
+Su correo es: " . 
+$useremail;
 
 //Create a new PHPMailer instance
 $mail = new PHPMailer;
@@ -29,7 +31,7 @@ $mail->isSMTP();
 // SMTP::DEBUG_OFF = off (for production use)
 // SMTP::DEBUG_CLIENT = client messages
 // SMTP::DEBUG_SERVER = client and server messages
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->SMTPDebug = 0;
 
 $mail->SMTPOptions = array(
 'ssl' => array(
@@ -57,7 +59,7 @@ $mail->SMTPAuth = true;
 $mail->Username = 'aabreu@ombutech.net';
 
 //Password to use for SMTP authentication
-$mail->Password = '';
+$mail->Password = '96033527a';
 
 //Set who the message is to be sent from --> CORREO DEL GREY
 $mail->setFrom('aabreu@ombutech.net', $usuarioname);
@@ -74,39 +76,19 @@ EOT;
 //convert HTML into a basic plain-text alternative body
 
 $mail->isHTML(false);
-$mail->Body = $useremail;
+$mail->Body = $body;
 //$mail->msgHTML(file_get_contents('contents.html'), _DIR_);
 
 //Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
+//$mail->AltBody = 'This is a plain-text message body';
 
 //send the message, check for errors
 if (!$mail->send()) {
     echo 'Mailer Error: '. $mail->ErrorInfo;
 } else {
-    echo 'Message sent!';
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
-}
-
-//Section 2: IMAP
-//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
-//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
-//You can use imap_getmailboxes($imapStream, '/imap/ssl', '*' ) to get a list of available folders or labels, this can
-//be useful if you are trying to get this working on a non-Gmail IMAP server.
-function save_mail($mail)
-{
-    //You can change 'Sent Mail' to any other folder or tag
-    $path = '{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail';
-
-    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-    $imapStream = imap_open($path, $mail->Username, $mail->Password);
-
-    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    imap_close($imapStream);
-
-    return $result;
+    echo '<script>
+    alert("El Mensaje a sido enviado con exito!");
+    window.history.go(-1);
+    </script>';
+    
 }
